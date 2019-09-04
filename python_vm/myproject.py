8f442c0611460d942df0c303315114c8ddec8326
andrewreaganm <andrewreaganm@ou.edu>
2019-09-04 21:12:09 +0000

    up and running

---
+++ python_vm/myproject.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import random
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return str(random.randint(0, 1000000))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
