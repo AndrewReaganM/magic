20239a033f1240d718b1245e7766ed3ee070ebdc
Andrew Massey <andrewreaganm@ou.edu>
2019-09-23 08:54:33 -0500

    Update webreader.py
    * Added timeout to make program faster
    * Added a few print statements to make the status more visible.

--- webreader.py
+++ webreader.py
@@ -4,18 +4,22 @@ import yaml
 
 def readURL(url):
     start_time = time.time()
-    f = urlopen(url)
+    f = urlopen(url, timeout=3)
     myfile = f.read()
     end_time = time.time()
     return str(int(round((end_time-start_time)*1000, 0))) + "ms - " + url + "\n"
 
+print("Testing URLs...")
 stream = open("sites.yaml", "r")
 docs = yaml.load(stream, Loader=yaml.FullLoader)
 
 outputfile = open("outputfile.txt", "w")
 
 for doc in docs['sites']:
+    print("Testing " + doc)
     try:
         outputfile.write(readURL(doc))
     except:
-        outputfile.write("Timeout Error on " + doc)
+        outputfile.write("Timeout Error on " + doc + "\n")
+
+print("Testing Complete.\nResults written to outputfile.txt")
